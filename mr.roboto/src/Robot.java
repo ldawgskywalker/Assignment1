43f0615f1511d32c358dffe46039816dbb769326
Luke Creighton <lk479778@dal.ca>
2017-02-01 12:54:21 -0400

    ROBOT Started

---
+++ mr.roboto/src/Robot.java
@@ -0,0 +1,54 @@
+public class Robot {
+	private String name;
+	private int xpos, ypos;
+	private int dir;
+	private int pts;
+	
+	public Robot(String n, int d){
+		name = n;
+		dir = d;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getXpos() {
+		return xpos;
+	}
+
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
+
+	public int getYpos() {
+		return ypos;
+	}
+
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
+
+	public int getDir() {
+		return dir;
+	}
+
+	public void setDir(int dir) {
+		this.dir = dir;
+	}
+
+	public int getPts() {
+		return pts;
+	}
+
+	public void setPts(int pts) {
+		this.pts = pts;
+	}
+	
+	
+	
+}
