9960f9262b45437723fe84f84bff62014a0d861d
Luke Creighton <lk479778@dal.ca>
2017-02-01 14:04:30 -0400

    amIAhead fixed

--- mr.roboto/Robot.java
+++ mr.roboto/Robot.java
@@ -1,5 +1,5 @@
 public class Robot {
-	private String name;
+	private String name, d;
 	private int xpos, ypos;
 	private int dir;
 	private int pts;
@@ -11,7 +11,7 @@ public class Robot {
 		ypos = 1;
 		pts  = 0;
 	}
-
+	// GET and SET METHODS
 	public String getName() {
 		return name;
 	}
@@ -49,21 +49,61 @@ public class Robot {
 	}
 
 	public void setPts(int pts) {
-		this.pts = pts;
+	
+	public void stringDir(){
+		if(dir==1)
+			d = "UP";
+		if(dir==2)
+			d = "DOWN";
+		if(dir==3)
+			d = "DIAG";
 	}
 	
-	public void move(int m){
+	// END OF GET SETS //
+	
+	
+	
+	public void move(){
+		
+		int m = (int)Math.random()*3+1;
+		
 		//UP
-		if(dir==1)
+		if(dir==1){
+			while(m+ypos>8){
+				int m = (int)Math.random()*3+1;
+			}
 			ypos+=m;
+		}
 		//DIAGNAL
-		if(dir==2){
+		if(dir==3){
+			while(m+ypos>8 && m+xpos>8){
+				int m = (int)Math.random()*3+1;
+			}
 			xpos+=m;
 			ypos+=m;
 		}
 		//RIGHT
-		if(dir==3)
+		if(dir==2){
+			while(m+xpos>8){
+				int m = (int)Math.random()*3+1;			
+			}
 			xpos+=m;
+		}
+	}
+	public String toString(){
+		return "Name: "+name+ "("+xpos+", "+ypos+")"+
+				"\nX Position: "+xpos+"\tY Position: "+ypos+
+				"\nPoints Collected: "+pts;
+	}
+	
+	public String amIAhead(Robot r){
+		if(xpos+ypos>r.getXpos()+r.getYpos()){
+			return name+" is ahead!";
+		}
+		if(xpos+ypos<r.getXpos()+r.getYpos()){
+			return r.getName()+" is ahead!";
+		}
+		return "They are the same distance from the goal!";
 	}
 	
 	
